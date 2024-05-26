c2ae66619f6e2eb2ef3e8ea469e9681df2913b38
David <david@loki.local>
2024-05-26 13:16:32 -0400

    first darwin

--- flake.nix
+++ flake.nix
@@ -1,35 +1,72 @@
 {
-  description = "My first flake!";
+  description = "DARWIN-LOKI";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    darwin.url = "github:LnL7/nix-darwin";
     home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    hyprland.url = "github:hyprwm/Hyprland";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, hyprland, ... }:
+  outputs = { self, nixpkgs, home-manager, darwin, nix-homebrew, ... }:
     let
-      system = "x86_64-linux";
-      lib = nixpkgs.lib;
-      pkgs = nixpkgs.legacyPackages.${system};
+      system = "x86_64-darwin";
+      pkgs = import nixpkgs { system = system; config.allowUnfree = true; };
     in {
-      nixosConfigurations = {
-        BaldrNix = lib.nixosSystem {
-          inherit system;
+      # Define system-wide packages and configurations
+      darwinConfigurations = {
+        loki = darwin.lib.darwinSystem {
+          system = "x86_64-darwin";
           modules = [
-            ./configuration.nix
+            {
+              # System packages
+              environment.systemPackages = with pkgs; [
+                vim
+                zoxide
+                thefuck
+                ranger
+                neovim
+                alacritty
+                tmux
+                home-manager
+                neofetch
+                python3
+                bitwarden-cli
+		nerdfonts
+		lazygit
+
+              ];
+              services.nix-daemon.enable = true;
+              system.stateVersion = 4;
+            }
           ];
         };
       };
 
-     homeConfigurations = {
-        david = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [./home.nix ];
-        };
-      };
-    };
-}
+ modules = [
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
 
+            # User owning the Homebrew prefix
+            user = "david";
+
+            # Automatically migrate existing Homebrew installations
+            autoMigrate = true;
+          };
+        }
+      ];
+    };
+  
+}
 
--- home.nix
+++ home.nix
@@ -1,102 +1,30 @@
 { config, pkgs, ... }:
-let
-   myAliases = {
-      	nix-switch = "sudo nixos-rebuild switch --flake /home/david/.dotnix/.";
-         nix-boot = "sudo nixos-rebuild boot --flake /home/david/.dotnix/.";
-         nix-test = "sudo nixos-rebuild test --flake /home/david/.dotnix/.";
 
-	      home = "home-manager switch --flake /home/david/.dotnix/.";
-         fixnix = "nvim /home/david/.dotnix/configuration.nix";
-	      fixhome = "nvim /home/david/.dotnix/home.nix";
-	      update = "sudo nixos-rebuild switch --flake /home/david/.dotnix/. && home-manager switch --flake /home/david/.nixconfig/.";
-         nf = "neofetch";
-         trash = "sudo nix-collect-garbage -d";
-      
-
-   };
-in
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+  # Set your username and home directory
   home.username = "david";
-  home.homeDirectory = "/home/david";
+  home.homeDirectory = "/Users/david";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "22.11"; # Please read the comment before changing.
+  # Enable Home Manager
+  programs.home-manager.enable = true;
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-     pkgs.hello
-     pkgs.thefuck
-     pkgs.ranger
+  # Specify the state version for compatibility
+  home.stateVersion = "21.05"; 
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
+  # Define additional file configurations if necessary
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
+    # Example:
+    # ".zshrc".text = ''
+    #   export PATH=$HOME/.local/bin:$PATH
+    #   # Add more custom configurations here
     # '';
   };
 
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/david/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
+  # Define session variables if needed
   home.sessionVariables = {
-     EDITOR = "neovim";
+    # Example:
+     EDITOR = "nvim";
+    # Add more environment variables here
   };
-
-
-
-  programs.zsh = {
-     enable = true;
-     shellAliases = myAliases;
-  };
-
-  programs.bash = {
-     enable = true;
-     shellAliases = myAliases;
-
-  };
-
-
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
+
